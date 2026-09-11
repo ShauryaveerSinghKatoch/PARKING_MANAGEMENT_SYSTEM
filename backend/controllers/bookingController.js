4dd7e317c97f8f4f4504f6d720c44d9418d9e3c6
Chandan Kumar <rkr15803@gmail.com>
2026-09-11 23:28:57 +0530

    Refactor Razorpay initialization
    Conditionally initialize Razorpay based on environment variables.

--- backend/controllers/bookingController.js
+++ backend/controllers/bookingController.js
@@ -4,10 +4,14 @@ const User = require('../models/User');
 const crypto = require('crypto');
 const Razorpay = require('razorpay');
 
-const razorpay = new Razorpay({
+// const razorpay = new Razorpay({
+//   key_id: process.env.RAZORPAY_KEY_ID,
+//   key_secret: process.env.RAZORPAY_KEY_SECRET
+// });
+const razorpay = process.env.RAZORPAY_KEY_ID ? new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET
-});
+}) : null;
 
 // Create Razorpay Order
 exports.createOrder = async (req, res) => {
